e803fd0458f5d71ec82c722252f505b50212e063
Tomas7855 <Tomasromero867@gmail.com>
2023-05-08 15:42:17 -0600

    creacion de clase java

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/FacultadMenuActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ues.fia.eisi.cafetinesues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FacultadMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facultad_menu);
+    }
+}
