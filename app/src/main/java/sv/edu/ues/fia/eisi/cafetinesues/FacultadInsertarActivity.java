f2ce53b3cff7b7156cd5d179abb9fc54870aa6da
Tomas7855 <Tomasromero867@gmail.com>
2023-05-10 16:17:42 -0600

    CRUD facultad terminado

--- app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/FacultadInsertarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/FacultadInsertarActivity.java
@@ -1,14 +1,50 @@
 package sv.edu.ues.fia.eisi.cafetinesues;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class FacultadInsertarActivity extends AppCompatActivity {
+public class FacultadInsertarActivity extends Activity {
+
+    ControlDB helper;
+    EditText codigo_Facultad;
+    EditText nombre_Facultad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facultad_insertar);
+
+        helper = new ControlDB(this);
+        codigo_Facultad= (EditText) findViewById(R.id.id_Facultad);
+        nombre_Facultad = (EditText) findViewById(R.id.nombre_Facultad);
+
     }
+
+    public void insertarFacultad(View v)
+    {
+        String id_Facultad = codigo_Facultad.getText().toString();
+        String nom_Facultad = nombre_Facultad.getText().toString();
+        Facultad facultad = new Facultad();
+
+        facultad.setId_Faculdad(Integer.parseInt(id_Facultad));
+        facultad.setNombre_Facultad(nom_Facultad);
+        String regInsertados;
+
+        helper.abrir();
+        regInsertados=helper.Insertar(facultad);
+        helper.cerrar();
+        Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();;
+
+    }
+
+    public void limpiarTexto(View v){
+
+        codigo_Facultad.setText("");
+        nombre_Facultad.setText("");
+
+    }
+
 }
--- app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/FacultadMenuActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/FacultadMenuActivity.java
@@ -2,13 +2,41 @@ package sv.edu.ues.fia.eisi.cafetinesues;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+public class FacultadMenuActivity extends ListActivity {
 
-public class FacultadMenuActivity extends AppCompatActivity {
+    String[] menu = {"Insertar Facultad","Eliminar Facultad",
+            "Consultar Facultad","Actualizar Facultad"};
 
+    String[] activities = {"FacultadInsertarActivity", "FacultadEliminarActivity",
+            "FacultadConsultarActivity","FacultadActualizarActivity" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_facultad_menu);
+        ListView listView = getListView();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menu);
+        setListAdapter(adapter);
+    }
+
+    protected void onListItemClick(ListView l,View v, int position, long id){
+        super.onListItemClick(l,v,position,id);
+        String nombreValue= activities[position];
+
+        l.getChildAt(position).setBackgroundColor(Color.rgb(128,128,255));
+
+        try{
+            Class<?> clase = Class.forName("sv.edu.ues.fia.eisi.cafetinesues."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
     }
 }
