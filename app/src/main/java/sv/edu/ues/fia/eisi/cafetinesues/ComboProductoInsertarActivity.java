55f4c71d0ad2ff67ae9120452d910ff6727efe66
Daiv DV <daiv@gmail.com>
2023-05-27 10:49:55 -0600

    servicios dv v1.0

--- app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/ComboProductoInsertarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/ComboProductoInsertarActivity.java
@@ -3,9 +3,12 @@ package sv.edu.ues.fia.eisi.cafetinesues;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONObject;
 
 public class ComboProductoInsertarActivity extends AppCompatActivity {
 
@@ -14,10 +17,15 @@ public class ComboProductoInsertarActivity extends AppCompatActivity {
     EditText editIdProducto;
     ControlDB helper;
 
+    private final String urlLocal = "http://192.168.1.45/ws_comboproducto_insert.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combo_producto_insertar);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         helper = new ControlDB(this);
         editIdComboProducto = findViewById(R.id.id_ComboProducto);
         editIdCombo = findViewById(R.id.id_Combo);
@@ -42,6 +50,15 @@ public class ComboProductoInsertarActivity extends AppCompatActivity {
             Toast.makeText(this, "Error en los datos ingresados", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    public void insertarComboProductoServicio(View v){
+        String url = null;
+        JSONObject datosComboProducto = new JSONObject();
+        JSONObject comboProducto = new JSONObject();
+        url = urlLocal+ "?idComboProducto=" + editIdComboProducto.getText().toString() + "&idCombo="
+                + editIdCombo.getText().toString() + "&idProducto=" + editIdProducto.getText().toString();
+        ControladorServicio.insertarComboProductoExterno(url, this);
     }
 
     public void limpiarTextoInsertarComboProducto(View v){
--- app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/ControladorServicio.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/cafetinesues/ControladorServicio.java
@@ -2,90 +2,155 @@ package sv.edu.ues.fia.eisi.cafetinesues;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.util.EntityUtils;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ControladorServicio
 {
-    public static String obtenerRespuestaPeticion(String url, Context ctx)
-    {
-        String respuesta = " ";
 
-        // Estableciendo tiempo de espera del servicio
-        HttpParams parametros = new BasicHttpParams();
-        HttpConnectionParams.setConnectionTimeout(parametros, 13000);
-        HttpConnectionParams.setSoTimeout(parametros, 15000);
+    public static String obtenerRespuestaPeticion(String url, Context ctx) {
+        String respuesta = "";
 
-        // Creando objetos de conexion
-        HttpClient cliente = new DefaultHttpClient(parametros);
-        HttpGet httpGet = new HttpGet(url);
         try {
-            HttpResponse httpRespuesta = cliente.execute(httpGet);
-            StatusLine estado = httpRespuesta.getStatusLine();
-            int codigoEstado = estado.getStatusCode();
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setConnectTimeout(13000);
+            conn.setReadTimeout(15000);
+            conn.setRequestMethod("GET");
+
+            int codigoEstado = conn.getResponseCode();
             String esta = String.valueOf(codigoEstado);
             Toast.makeText(ctx, esta, Toast.LENGTH_LONG).show();
-            if (codigoEstado == 200) {
-                HttpEntity entidad = httpRespuesta.getEntity();
-                respuesta = EntityUtils.toString(entidad);
+
+            if (codigoEstado == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String linea;
+                while ((linea = reader.readLine()) != null) {
+                    stringBuilder.append(linea);
+                }
+                reader.close();
+                respuesta = stringBuilder.toString();
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             Toast.makeText(ctx, url, Toast.LENGTH_LONG).show();
-            Toast.makeText(ctx, "Error en la conexion", Toast.LENGTH_LONG)
-                    .show();
-            // Desplegando el error en el LogCat
-            Log.v("Error de Conexion", e.toString());
+            Toast.makeText(ctx, "Error en la conexión", Toast.LENGTH_LONG).show();
+            Log.v("Error de Conexión", e.toString());
         }
+
         return respuesta;
     }
 
-    public static String obtenerRespuestaPost(String url, JSONObject obj, Context ctx)
-    {
-        String respuesta = " ";
-        try {
-            HttpParams parametros = new BasicHttpParams();
-            HttpConnectionParams.setConnectionTimeout(parametros, 3000);
-            HttpConnectionParams.setSoTimeout(parametros, 5000);
-            HttpClient cliente = new DefaultHttpClient(parametros);
-            HttpPost httpPost = new HttpPost(url);
-            httpPost.setHeader("content-type", "application/json");
-            StringEntity nuevaEntidad = new StringEntity(obj.toString());
-            httpPost.setEntity(nuevaEntidad);
-            Log.v("Peticion", url);
-            Log.v("POST", httpPost.toString());
-            HttpResponse httpRespuesta = cliente.execute(httpPost);
-            StatusLine estado = httpRespuesta.getStatusLine();
 
-            int codigoEstado = estado.getStatusCode();
-            if (codigoEstado == 200) {
-                respuesta = Integer.toString(codigoEstado);
-                Log.v("respuesta", respuesta);
+    public static String obtenerRespuestaPost(String url, JSONObject obj, Context ctx) {
+        String respuesta = "";
+
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setConnectTimeout(3000);
+            conn.setReadTimeout(5000);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            String jsonInputString = obj.toString();
+
+            try (OutputStream outputStream = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                outputStream.write(input, 0, input.length);
+            }
+
+            int codigoEstado = conn.getResponseCode();
+
+            if (codigoEstado == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String linea;
+                while ((linea = reader.readLine()) != null) {
+                    stringBuilder.append(linea);
+                }
+                reader.close();
+                respuesta = stringBuilder.toString();
             } else {
                 Log.v("respuesta", Integer.toString(codigoEstado));
             }
-        } catch (Exception e) {
-            Toast.makeText(ctx, "Error en la conexion", Toast.LENGTH_LONG)
-                    .show();
-            // Desplegando el error en el LogCat
-            Log.v("Error de Conexion", e.toString());
+        } catch (IOException e) {
+            Toast.makeText(ctx, "Error en la conexión", Toast.LENGTH_LONG).show();
+            Log.v("Error de Conexión", e.toString());
         }
+
         return respuesta;
     }
 
+    //
+    //
+    // ESTE ES SOLO DE EJEMPLO DE LA GUIA 8
+    //
+    //
+    public static void insertarNotaExterno(String peticion, Context ctx) {
+        String json = obtenerRespuestaPeticion(peticion, ctx);
+        try {
+            JSONObject resultado = new JSONObject(json);
+
+            Toast.makeText(ctx, "Registro ingresado"+ resultado.getJSONArray("resultado"), Toast.LENGTH_LONG)
+                    .show();
+            int respuesta = resultado.getInt("resultado");
+            if (respuesta == 1)
+                Toast.makeText(ctx, "Registro ingresado", Toast.LENGTH_LONG)
+                        .show();
+            else
+                Toast.makeText(ctx, "Error registro duplicado",
+                        Toast.LENGTH_LONG).show();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void insertarComboProductoExterno(String peticion, Context ctx){
+        String json = obtenerRespuestaPeticion(peticion, ctx);
+        try {
+            JSONObject resultado = new JSONObject(json);
+
+            int respuesta = resultado.getInt("resultado");
+            if (respuesta == 1)
+                Toast.makeText(ctx, "Registro ingresado", Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(ctx, "Error registro duplicado",
+                        Toast.LENGTH_LONG).show();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String consultarAparicionesProducto(String json, Context ctx){
+        try {
+            Log.v("JSON", "averaquiprimeralinea");
+            JSONObject objs = new JSONObject(json);
+            Log.v("JSON", "aaaaa" + objs);
+            if (objs.length() != 0)
+                // CANTIDAD APARICIONES
+                return objs.getString("cantidadComboProducto");
+            else {
+                Toast.makeText(ctx, "Error", Toast.LENGTH_LONG)
+                        .show();
+                return " ";
+            }
+        } catch (JSONException e) {
+            Toast.makeText(ctx, "Error con la respuesta JSON",
+                    Toast.LENGTH_LONG).show();
+            return " ";
+        }
+    }
 }
